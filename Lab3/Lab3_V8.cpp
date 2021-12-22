81cfffa5ab8355a96859fce7b7ce3cef6a38ed89
Roma <sikarev.roman@yandex.ru>
2021-12-22 22:54:29 +0300

    Финал, с комментариями

--- Lab3/Lab3_V8.cpp
+++ Lab3/Lab3_V8.cpp
@@ -1,39 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
 #include "Utils.h"
 
 using namespace std;
 
 int main() {
+	srand(time(0));
+
+	const int SIZE = 4;
+
 	string path = "data.txt";
+	string output = "output.txt";
 
-	const int SIZE = 3;
-	Matrix<>A(SIZE), H(SIZE);
-
-	Matrix<double> lambda(1, SIZE);
-	for (int i = 0; i < SIZE; i++) {
-		lambda(0, i) = i + 1;
-	}
-	Matrix<double> omega(1, SIZE);
-	for (int i = 0; i < SIZE; i++) {
-		omega(0, i) = (double(rand() % 10 + 1));
-	}
-
-	Matrix<>l_v(SIZE, 1);
-
-	double eps_v = 0.1;
-	double eps_l = 0.1;
+	double eps_v, eps_l, r;
+	eps_v = eps_l = 1;
 	int index = 0, iterations = 40, k_v = -1, k_l = -1;
+	
 	///////////////
-	buildTestMatrix(A, H, lambda, omega, eps_l, l_v, index);
+
+	int begin = 1;
+	int end = 10;
+
+	// Тестовая матрица
+	Matrix<>A(SIZE), H(SIZE);
+	// Вектор собственных значений
+	Matrix<double> lambda(1, SIZE);
+
+	buildTestMatrix(A, H, lambda, begin, end);
+
+	// Записываем исходные данные в файл
 	writeToFile(A, eps_l, eps_v, iterations, path);
 
 	double l = 0;
-
+	int count = 0;
 	Matrix<> xn(1, SIZE);
 
+	// Решение задачи методом обратных итераций с исчерпыванием
 	method(A, xn, iterations, eps_l, eps_v, k_l, k_v, l);
+
+	int iterationCount = k_l > k_v ? k_l : k_v;
+
+	// Вычисление мере точности решения спектральной задачи
+	precisionMeasure(A, xn, l, r);
+
+	// Запись результатов в файл
+	writeResults(xn, l, iterationCount, r, output);
 	
 	system("pause");
 	return 0;
--- Lab3/Utils.h
+++ Lab3/Utils.h
@@ -10,7 +10,7 @@ bool Khaletsky(Matrix<> A, Matrix<>& B, Matrix<>& C)
 	for (int j = 0; j < size; j++) {
 		for (int i = j; i < size; i++) {
 			double sum = 0;
-			for (int k = 0; k <= j - 1; k++) {
+			for (int k = 0; k < j - 1; k++) {
 				sum += B(i, k) * C(k, j);
 			}
 			B(i, j) = A(i, j) - sum;
@@ -65,19 +65,28 @@ double cosBetweenVectors(Matrix<> v1, Matrix<> v2)
 		result / (vectorLength(v1) * vectorLength(v2));
 
 }
-void buildTestMatrix(Matrix<>& A, Matrix<>& H, Matrix<>& lambda, Matrix<>& omega, double& e_l, Matrix<>& l_v, int index) {
+void buildTestMatrix(Matrix<>& A, Matrix<>& H, Matrix<>& lambda, int begin, int end) {
 	const int SIZE = A.rowsCount();
+
+	// Заполняем матрицу случайными значениями из диапазона end-begin
+	for (int i = 0; i < SIZE; i++) {
+		lambda(0, i) = rand() % end + begin;
+	}
+
+	// Случайный нормированный вектор
+	Matrix<double> omega(1, SIZE);
+	for (int i = 0; i < SIZE; i++) {
+		omega(0, i) = rand() % 2001 - 1000;
+	}
+	normalize(omega);
 	
-	// ������� � ������������ ���������� A �� ���������
+	// Диагональная матрица с собственными значениями матрицы А
 	Matrix<> Lambda(SIZE);
 	for (int i = 0; i < SIZE; i++) {
 		Lambda(i, i) = lambda(0, i);
 	}
 
-	// ��������� ������ 
-	normalize(omega);
-
-	// ��������� ������� �����������
+	// Матрица Хаусхолдера
 	H = Matrix<double>(SIZE);
 	for (int row = 0; row < H.rowsCount(); row++) {
 		for (int col = 0; col < H.colsCount(); col++) {
@@ -85,26 +94,8 @@ void buildTestMatrix(Matrix<>& A, Matrix<>& H, Matrix<>& lambda, Matrix<>& omega
 		}
 	}
 
-	// ���������� ������������� �������
+	// Создание тестировочной матрицы
 	A = H * Lambda * H.transpose();
-
-	// ����� ������������ �� ������ ������. �������� � �����. ��� �������
-	e_l = lambda(0, index);
-	for (int i = 0; i < SIZE; i++) {
-		if (abs(e_l) > abs(lambda(0, i))) {
-			e_l = lambda(0, i);
-			index = i;
-		}
-	}
-	l_v = H.column(index);
-
-	/*for (int i = 0; i < SIZE; i++) {
-		cout << A * H.column(i) << endl;
-	}
-	cout << "=============" << endl;
-	for (int i = 0; i < SIZE; i++) {
-		cout << H.column(i) * lambda[i] << endl;
-	}*/
 }
 void writeToFile(Matrix<>& A, double e_l, double e_v, double iterations, string path) {
 	ofstream out;
@@ -117,8 +108,10 @@ void writeToFile(Matrix<>& A, double e_l, double e_v, double iterations, string 
 		out << iterations;
 	}
 }
-bool findY(Matrix<> B, Matrix<> f, Matrix<>& y) {
+bool findY(Matrix<>& B, Matrix<>& f, Matrix<>& y) {
+
 	const int SIZE = B.colsCount();
+
 	for (int i = 0; i < SIZE; i++) {
 		double sum = f(0, i);
 		for (int j = 0; j <= i - 1; j++) {
@@ -130,10 +123,12 @@ bool findY(Matrix<> B, Matrix<> f, Matrix<>& y) {
 		}
 		y(0, i) = sum / B(i, i);
 	}
+	/*cout << y << "========" << endl;
+	cout << B;*/
 	// Вектор полностью обработан, возврат true
 	return true;
 }
-void findX(Matrix<> C, Matrix<> y, Matrix<> x) {
+void findX(Matrix<>& C, Matrix<>& y, Matrix<>& x) {
 	const int SIZE = C.colsCount();
 	for (int i = SIZE - 1; i >= 0; i--) {
 		double sum = y(0, i);
@@ -151,59 +146,106 @@ double vectProd(Matrix<>& v1, Matrix<>& v2) {
 	}
 	return res;
 }
-void method(Matrix<>& A, Matrix<>& xn, int M, double eps_l, double eps_v, int& k_l, int& k_v, double lambda) {
+void method(Matrix<>& A, Matrix<>& xn, int M, double eps_l, double eps_v, int& k_l, int& k_v, double& lambda) {
 	const int SIZE = A.colsCount();
 
-	Matrix<> f_1(1, SIZE);
+	Matrix<> f_1(1, SIZE, 1);
 	Matrix<> f_2(1, SIZE);
 	Matrix<> B(SIZE);			// Нижнетреугольная матрица LU-разложения
 	Matrix<> C(SIZE);			// Верхнетреугольная матрица LU-разложения
 	Matrix<> E(SIZE, SIZE, 1);	// Единичная матрица
 	Matrix<> e(1, SIZE, 1);		// Единичный вектор
 
+
+	// Производим LU-разложение, в случае успеха запускаем процесс итерации (строка 165)
 	bool continueToWork = Khaletsky(A, B, C);
 	int count = 0;
 	double a1 = 1, a2;
 
 	while (continueToWork) {
 		Matrix<> y(1, SIZE), x(1, SIZE);
+
+		// Если удаось найти Y, ищем X
 		if (findY(B, f_1, y)) {
 			findX(C, y, x);
 		}
 
+		// Запоминаем предудущее собственное значение, начиная со второй итерации
 		if (count >= 2) {
 			a2 = a1;
 		}
 		a1 = vectProd(f_1, x);
 
+		// Запоминаем предыдущее значение собственного вектора, начиная со второй итерации
 		if (count >= 2) {
 			f_2 = f_1;
 		}
 		f_1 = x;
+
+		// Нормируем вектор
 		normalize(f_1);
 
 		count++;
+
+		// Начиная с третьей итерации
 		if (count >= 3) {
+			// Если мера собственный достигла требуемой точности, запоминаем потребовавшееся количетсво итераций
 			if (abs(a1 - a2) < eps_l) {
 				k_l = count;
 			}
 
+			// Если угол между векторами достиг требуемой точности, запоминаем потребовавшееся количетсво итераций
 			if (abs(cosBetweenVectors(f_1, f_2)) < eps_v) {
 				k_v = count;
 				if (k_l == -1) {
 					k_l = count;
 				}
+				// Завершаем процесс итерации
 				continueToWork = false;
 			}
 		}
 
+		// Завершаем процесс итераций, если превышено максимальное их количество
 		if (count >= M) {
 			continueToWork = false;
 		}
 	}
 
+	// Если удалось достичь требуемой точности, записываем полученные 
 	if (k_v != -1) {
 		lambda = 1 / a2;
 		xn = f_1;
+	}
+}
+// Поиск максимального по модулю отклонение компоненты вектора Ax − λx от нуля
+void precisionMeasure(Matrix<>& A, Matrix<>& x, double l, double& r) {
+	const int SIZE = A.colsCount();
+
+	Matrix<> b(1, SIZE);
+
+	b = A * x;
+
+	// Записываем отклонени я векторов Ax − λx от нуля
+	for (int i = 0; i < SIZE; i++) {
+		b(0, i) = b(0, i) - l * x(0, i);
+	}
+
+	r = b(0, 0);
+
+	// Находим максимальное отклонение среди полученных
+	for (int i = 1; i < SIZE; i++) {
+		if (b(0, i) > r) {
+			r = b(0, i);
+		}
+	}
+}
+void writeResults(Matrix<>& x, double l, double count, double r, string path) {
+	ofstream out;
+	out.open(path);
+	if (out.is_open()) {
+		out << l << endl;
+		out << x;
+		out << count << endl;
+		out << r << endl;
 	}
 }
